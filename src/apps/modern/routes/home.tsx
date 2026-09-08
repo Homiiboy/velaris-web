8ac4808e8c179168baffee39e929cc28dcc22464
Sandro <96988701+Homiiboy@users.noreply.github.com>
2026-09-08 14:34:55 +0200

    feat(velaris): show Profiles 2.0 startup gate

--- src/apps/modern/routes/home.tsx
+++ src/apps/modern/routes/home.tsx
@@ -6,6 +6,7 @@ import VelarisFranchiseShelf from 'apps/modern/features/franchises/VelarisFranch
 import VelarisHomeDestinations from 'apps/modern/features/home/VelarisHomeDestinations';
 import VelarisHomeHero from 'apps/modern/features/home/VelarisHomeHero';
 import VelarisSmartHome from 'apps/modern/features/home/VelarisSmartHome';
+import VelarisProfileGate from 'apps/modern/features/profiles/VelarisProfileGate';
 import {
     isVelarisHomeTabIndex,
     parseVelarisHomeTabIndex,
@@ -176,6 +177,7 @@ const Home = () => {
 
     return (
         <div ref={element}>
+            <VelarisProfileGate />
             <Page
                 id='indexPage'
                 className='mainAnimatedPage homePage libraryPage allLibraryPage pageWithAbsoluteTabs withTabs velaris-home-page'
